b53e6ade879a25fe644c91515bc0edc21e4702c9
Daniel <Daniel@DESKTOP-372O2U5>
2019-08-24 14:15:23 -0500

    Add User entities

--- MyVet.Web/Data/Entities/Owner.cs
+++ MyVet.Web/Data/Entities/Owner.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MyVet.Web.Data.Entities
 {
     public class Owner
-    { 
-        prop
+    {
+        public string Id { get; set; }
+
+        public User User { get; set; }
+
+        public ICollection<Pet> Pets { get; set; }
+
+        public ICollection<Agenda> Agendas { get; set; }
     }
 }
---
+++ MyVet.Web/Data/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyVet.Web.Data.Entities
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; internal set; }
+        public string LastName { get; internal set; }
+        public string Address { get; internal set; }
+        public string Document { get; internal set; }
+    }
+}
